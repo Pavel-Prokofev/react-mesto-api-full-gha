a6eb162d66949f4950f55fc381596791b050659b
Pavel Prokofev <kirgadan@yandex.ru>
2023-05-20 21:35:50 +0300

    Проектная работа 15, проба ВМ, nfyws c   CORS 1

--- backend/middlewares/allowedCors.js
+++ backend/middlewares/allowedCors.js
@@ -1,7 +1,6 @@
 const allowedCors = [
-  'http://localhost:3001',
-  'http://mesto.kirgadan.nomoredomains.monster/',
-  'https://mesto.kirgadan.nomoredomains.monster/',
+  'http://mesto.kirgadan.nomoredomains.monster',
+  'https://mesto.kirgadan.nomoredomains.monster',
 ];
 
 module.exports = {
